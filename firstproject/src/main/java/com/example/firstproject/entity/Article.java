7b6d968a0223d57365957cfbbf1fe018ba1e032b
jogyueun <55841366+jogyueun@users.noreply.github.com>
2024-01-30 19:50:10 +0900

    01-29

--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -7,11 +7,11 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-@Entity
-@Getter
+@AllArgsConstructor // 생성자 롬복으로 추가
+@NoArgsConstructor  // 기본 생성자 롬복으로 추가
+@ToString           // @toString 메서드 롬복으로 기본 추가
+@Entity             // 현재 클래스가 엔티티 라는 것을 명시
+@Getter             // getter 메서드 롬복으로 추가
 public class Article {
 
     @Id
@@ -25,4 +25,10 @@ public class Article {
     private String content;
 
 
+    public void patch(Article article) {
+        if (article.title != null)
+            this.title = article.title;
+        if (article.content != null)
+            this.content = article.content;
+    }
 }

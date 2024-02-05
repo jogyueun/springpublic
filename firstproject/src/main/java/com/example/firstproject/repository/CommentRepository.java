43dc8c1d87453f7b831c4873120ea34d1c8f470c
jogyueun <55841366+jogyueun@users.noreply.github.com>
2024-02-05 18:24:12 +0900

    add:

--- firstproject/src/main/java/com/example/firstproject/repository/CommentRepository.java
+++ firstproject/src/main/java/com/example/firstproject/repository/CommentRepository.java
@@ -10,8 +10,8 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     // 특정 게시글의 모든 댓글 조회
     @Query(value =
-            "SELECT *" +
-            " FROM comment " +
+            "SELECT * " +
+            "FROM comment " +
             "WHERE article_id = :articleId",
             nativeQuery = true)
     List<Comment> findByArticleId(@Param("articleId") Long articleId);
